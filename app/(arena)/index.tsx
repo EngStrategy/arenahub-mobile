191ad0d41773634f6501ba12c79879f75aa6dbf6
Rian Lima <rlimamendes@proton.me>
2025-11-23 17:03:31 -0300

    Adição de botão de logout no appheader

--- app/(arena)/index.tsx
+++ app/(arena)/index.tsx
@@ -1,9 +1,15 @@
 import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { AppHeader } from '@/components/layout/AppHeader';
 
 export default function DashboardScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#FFF' }}>
-      <Text>Dashboard de arena</Text>
-    </View>
-  );
+      <SafeAreaView className="flex-1 bg-white" edges={['top']}>
+        <AppHeader/>
+
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#FFF' }}>
+            <Text>Dashboard da arena</Text>
+        </View>
+      </SafeAreaView>
+    );
 }
